1446379bc513d5d93c514ee6b09a0bed44f0a046
Your Name <you@example.com>
2019-11-09 08:17:40 +0530

    code base added

---
+++ spring-boot-k8s-app-2/src/main/java/com/springboot/k8s/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.springboot.k8s.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TestController {
+
+	@GetMapping(path = "/app2", produces = "application/json")
+	public ResponseEntity<String> dockerTest() {
+		return ResponseEntity.status(HttpStatus.OK).body("Welcome to spring boot k8s example:App2");
+	}
+
+}
